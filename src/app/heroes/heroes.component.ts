9f9c9540b57c078211708fa7078fd332e43bff2d
Coo1PeR <coo2per@gmail.com>
2024-05-03 14:37:12 +0300

    feat: create separete hero-detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import { Hero } from "../hero";
 import { NgIf, UpperCasePipe, NgFor } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { HEROES } from "../mock-heroes";
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 
 @Component({
   selector: "app-heroes",
@@ -11,7 +12,8 @@ import { HEROES } from "../mock-heroes";
     UpperCasePipe,
     FormsModule,
     NgFor,
-    NgIf
+    NgIf,
+    HeroDetailComponent
   ],
   templateUrl: "./heroes.component.html",
   styleUrl: "./heroes.component.css"
